6b0e5acddbe700ef4a3ef686fb3fd125314aa470
hussa0 <59232894+hussa0@users.noreply.github.com>
2026-01-10 14:48:17 +0000

    rm kitty, add install bash

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+ln -sf ~/projects/dotfiles/wezterm/wezterm.lua ~/.wezterm.lua
+ln -sf ~/projects/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/projects/dotfiles/git/.gitconfig ~/.gitconfig
+
